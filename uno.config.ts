39fb9e1fdcef3c2b2ec7409a6f623e6173be75f7
zimo493 <2081182432@qq.com>
2025-11-06 08:58:26 +0800

    refactor: :recycle: (config)重构 `ESLint` 和 `Stylelint` 配置文件
    - 将 ESLint 配置从 JavaScript 迁移至 TypeScript
    
    - 更新 Stylelint 继承配置并添加 Prettier 插件
    
    - 调整 TypeScript 模块解析策略为 bundler
    
    - 新增 UnoCSS 配置文件并移除旧版配置
    
    - 更新 commitlint 配置中的部分文本提示内容
    
    - 在 tsconfig.json 中包含新的配置文件路径
    
    - 优化 Vue 和 TypeScript 相关的 ESLint 规则配置

---
+++ uno.config.ts
@@ -0,0 +1,31 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetWind3,
+  presetTypography,
+  transformerDirectives,
+  transformerVariantGroup,
+} from "unocss";
+
+// https://github.com/unocss/unocss
+export default defineConfig({
+  presets: [
+    presetWind3(), // UnoCSS 的默认预设
+    presetAttributify(), // 属性化模式预设，允许你通过属性来使用类名
+    presetIcons(), // 图标预设
+    presetTypography(), // 版预设
+  ],
+  // 自定义快捷类
+  shortcuts: {
+    "wh-full": "w-full h-full",
+    "flex-center": "flex justify-center items-center",
+    "flex-col-center": "flex-center flex-col",
+    "flex-x-center": "flex justify-center",
+    "flex-y-center": "flex items-center",
+  },
+  transformers: [
+    transformerDirectives(), // 启用指令转换器，允许动态绑定 UnoCSS 类
+    transformerVariantGroup(), // 启用变体分组转换器，允许对样式变体进行分组。
+  ],
+});
